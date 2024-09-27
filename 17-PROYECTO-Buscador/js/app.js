1353fa258d94b6cb0011f052befbe889c85cb148
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-09-27 12:37:28 -0500

    "feat: add function to populate year select element"

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,16 +1,29 @@
 const result = document.querySelector("#resultado");
+const year = document.querySelector("#year");
+const maxYear = new Date().getFullYear();
+const minYear = maxYear - 10;
 
 document.addEventListener("DOMContentLoaded", () => {
   showCars();
+  fillYearSelect();
 });
 
 function showCars() {
   cars.forEach((car) => {
-    const {marca, modelo, year, puertas, transmision, precio, color} = car;
+    const { marca, modelo, year, puertas, transmision, precio, color } = car;
     const carHTML = document.createElement("p");
-  
+
     carHTML.textContent = `${marca} - ${modelo} - ${year} - ${puertas} Puertas - Transmision: ${transmision} - Precio: ${precio} - Color: ${color}`;
 
     result.appendChild(carHTML);
   });
+}
+
+function fillYearSelect() {
+  for (let i = maxYear; i >= minYear; i--) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    year.appendChild(option);
+  }
 }
