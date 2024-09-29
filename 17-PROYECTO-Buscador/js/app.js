5e85c0cfd16617d139e9a3989dff1d893dfe97f5
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-09-28 23:47:35 -0500

    feat: add filtering functions for cars by brand

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,11 +1,58 @@
-const result = document.querySelector("#resultado");
+const brand = document.querySelector("#marca");
 const year = document.querySelector("#year");
+const minPrice = document.querySelector("#minimo");
+const maxPrice = document.querySelector("#maximo");
+const doors = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+const result = document.querySelector("#resultado");
 const maxYear = new Date().getFullYear();
 const minYear = maxYear - 10;
+
+const dataSearch = {
+  marca: "",
+  year: "",
+  minimo: "",
+  maximo: "",
+  puertas: "",
+  transmision: "",
+  color: "",
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   showCars();
   fillYearSelect();
+});
+
+brand.addEventListener("change", (e) => {
+  dataSearch.marca = e.target.value;
+  filterCars();
+  console.log(dataSearch);
+});
+
+year.addEventListener("change", (e) => {
+  dataSearch.year = e.target.value;
+});
+
+minPrice.addEventListener("change", (e) => {
+  dataSearch.minimo = e.target.value;
+});
+
+maxPrice.addEventListener("change", (e) => {
+  dataSearch.maximo = e.target.value;
+});
+
+doors.addEventListener("change", (e) => {
+  dataSearch.puertas = e.target.value;
+});
+
+transmision.addEventListener("change", (e) => {
+  dataSearch.transmision = e.target.value;
+});
+
+color.addEventListener("change", (e) => {
+  dataSearch.color = e.target.value;
+  console.log(dataSearch);
 });
 
 function showCars() {
@@ -26,4 +73,17 @@ function fillYearSelect() {
     option.textContent = i;
     year.appendChild(option);
   }
+}
+
+function filterCars() {
+  const result = cars.filter(filterBrand);
+  console.log(result)
+}
+
+function filterByBrand(car) {
+  const {marca} = dataSearch
+  if (marca) {
+    return car.marca === marca;
+  }
+  return car;
 }
