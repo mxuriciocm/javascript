d784d15e1ebff5fc7c8459c3df46f6bfc880fea1
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-09-29 21:55:35 -0500

    feat: add filtering by doors, color, prices, transmission and displayed in real time

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -20,42 +20,48 @@ const dataSearch = {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  showCars();
+  showCars(cars);
   fillYearSelect();
 });
 
 brand.addEventListener("change", (e) => {
   dataSearch.marca = e.target.value;
   filterCars();
-  console.log(dataSearch);
 });
 
 year.addEventListener("change", (e) => {
   dataSearch.year = e.target.value;
+  filterCars();
 });
 
 minPrice.addEventListener("change", (e) => {
   dataSearch.minimo = e.target.value;
+  filterCars();
 });
 
 maxPrice.addEventListener("change", (e) => {
   dataSearch.maximo = e.target.value;
+  filterCars();
 });
 
 doors.addEventListener("change", (e) => {
   dataSearch.puertas = e.target.value;
+  filterCars();
 });
 
 transmision.addEventListener("change", (e) => {
   dataSearch.transmision = e.target.value;
+  filterCars();
 });
 
 color.addEventListener("change", (e) => {
   dataSearch.color = e.target.value;
+  filterCars();
   console.log(dataSearch);
 });
 
-function showCars() {
+function showCars(cars) {
+  clearHTML();
   cars.forEach((car) => {
     const { marca, modelo, year, puertas, transmision, precio, color } = car;
     const carHTML = document.createElement("p");
@@ -64,6 +70,12 @@ function showCars() {
 
     result.appendChild(carHTML);
   });
+}
+
+function clearHTML() {
+  while (result.firstChild) {
+    result.removeChild(result.firstChild);
+  }
 }
 
 function fillYearSelect() {
@@ -76,14 +88,81 @@ function fillYearSelect() {
 }
 
 function filterCars() {
-  const result = cars.filter(filterBrand);
-  console.log(result)
+  const result = cars
+    .filter(filterByBrand)
+    .filter(filterByYear)
+    .filter(filterByNumberOfDoors)
+    .filter(filterByTransmision)
+    .filter(filterByColor)
+    .filter(filterByMaxPrice)
+    .filter(filterByMinPrice);
+  if (result.length) {
+    showCars(result);
+  } else {
+    showMessage();
+  }
+}
+
+function showMessage() {
+  clearHTML();
+  const message = document.createElement("div");
+  message.classList.add("alerta", "error");
+  message.textContent = "No hay resultado";
+  result.appendChild(message);
 }
 
 function filterByBrand(car) {
-  const {marca} = dataSearch
+  const { marca } = dataSearch;
   if (marca) {
     return car.marca === marca;
+  }
+  return car;
+}
+
+function filterByYear(car) {
+  const { year } = dataSearch;
+  if (year) {
+    return car.year === parseInt(year);
+  }
+  return car;
+}
+
+function filterByMinPrice(car) {
+  const { minimo } = dataSearch;
+  if (minimo) {
+    return car.precio >= minimo;
+  }
+  return car;
+}
+
+function filterByMaxPrice(car) {
+  const { maximo } = dataSearch;
+  if (maximo) {
+    return car.precio <= maximo;
+  }
+  return car;
+}
+
+function filterByNumberOfDoors(car) {
+  const { puertas } = dataSearch;
+  if (puertas) {
+    return car.puertas === parseInt(puertas);
+  }
+  return car;
+}
+
+function filterByTransmision(car) {
+  const { transmision } = dataSearch;
+  if (transmision) {
+    return car.transmision === transmision;
+  }
+  return car;
+}
+
+function filterByColor(car) {
+  const { color } = dataSearch;
+  if (color) {
+    return car.color === color;
   }
   return car;
 }
