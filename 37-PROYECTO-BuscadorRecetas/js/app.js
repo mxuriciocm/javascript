e73defce1c34de6c275ea7c2f1311595e01c170b
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-12-02 22:45:28 -0500

    feat: add main recipe search page with category selection and modal for recipe details

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,8 +1,10 @@
 const categoryInput = document.querySelector("#categorias");
 const result = document.querySelector("#resultado");
+const modal = new bootstrap.Modal("#modal", {});
 
 window.addEventListener("load", () => {
   categoryInput.addEventListener("change", fetchMealsByCategories);
+
   const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
   fetch(url)
     .then((response) => response.json())
@@ -29,27 +31,128 @@ function fetchMealsByCategories(e) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      clearHTML();
       showMealsByCategories(data.meals);
     })
     .catch((e) => console.log(e));
 }
 
-function showMealsByCategories(array) {
-  const cardMealContainer = document.createElement("div");
-  cardMealContainer.classList.add("flex");
-  array.forEach((element) => {
-    const { strMeal, strMealThumb, idMeal } = element;
-    const cardMeal = document.createElement("div");
-    cardMeal.innerHTML = `<img src="${strMealThumb}" alt="${strMeal}">
-                          <p>${strMeal}</p>`;
-    cardMealContainer.appendChild(cardMeal);
+function showMealsByCategories(recipes) {
+  clearHTML(result);
+  const heading = document.createElement("h2");
+  heading.classList.add("text-center", "text-black", "my-5");
+  heading.textContent = recipes.length ? "Results" : "No Results";
+  result.appendChild(heading);
+
+  recipes.forEach((recipe) => {
+    const { strMeal, strMealThumb, idMeal } = recipe;
+    const recipeContainer = document.createElement("div");
+    recipeContainer.classList.add("col-md-4");
+
+    const recipeCard = document.createElement("div");
+    recipeCard.classList.add("card", "mb-4");
+
+    const recipeImage = document.createElement("img");
+    recipeImage.classList.add("card-img-top");
+    recipeImage.alt = `Recipe image ${strMeal}`;
+    recipeImage.src = `${strMealThumb}`;
+
+    const recipeBody = document.createElement("div");
+    recipeBody.classList.add("card-body");
+
+    const recipeTitle = document.createElement("h3");
+    recipeTitle.classList.add("card-title", "mb-3");
+    recipeTitle.textContent = `${strMeal}`;
+
+    const recipeButton = document.createElement("button");
+    recipeButton.classList.add("btn", "btn-danger", "w-100", "btn-recipe");
+    recipeButton.textContent = "Show recipe";
+    recipeButton.dataset.bsTarget = "#modal";
+    recipeButton.dataset.bsToggle = "modal";
+    recipeButton.onclick = function () {
+      fetchRecipeInfoById(idMeal);
+    };
+
+    recipeBody.appendChild(recipeTitle);
+    recipeBody.appendChild(recipeButton);
+
+    recipeCard.appendChild(recipeImage);
+    recipeCard.appendChild(recipeBody);
+
+    recipeContainer.appendChild(recipeCard);
+
+    result.appendChild(recipeContainer);
   });
-  result.appendChild(cardMealContainer);
 }
 
-function clearHTML() {
-  while (result.firstChild) {
-    result.removeChild(result.firstChild);
+function fetchRecipeInfoById(id) {
+  fetch(`https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      showRecipeInfoById(data.meals[0]);
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+}
+
+function showRecipeInfoById(info) {
+  console.log(info);
+  const { strMeal, strCategory, strInstructions, strMealThumb } = info;
+  const modalTitle = document.querySelector(".modal .modal-title");
+  const modalBody = document.querySelector(".modal .modal-body");
+  modalTitle.textContent = strMeal;
+  modalBody.innerHTML = `
+    <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
+    <h3 class="my-3">Instrucciones</h3>
+    <p>${strInstructions}</p>
+    <h3 class="my-3">Ingredientes:</h3>
+  `;
+
+  const listGroup = document.createElement("ul");
+  listGroup.classList.add("list-group");
+  for (let i = 0; i < 20; ++i) {
+    if (info[`strIngredient${i}`]) {
+      const ingredient = info[`strIngredient${i}`];
+      const quantity = info[`strMeasure${i}`];
+
+      const ingredientsLi = document.createElement("li");
+      ingredientsLi.classList.add("list-group-item");
+      ingredientsLi.textContent = `${ingredient} - ${quantity}`;
+
+      listGroup.appendChild(ingredientsLi);
+    }
+  }
+
+  modalBody.appendChild(listGroup);
+
+  const modalFooter = document.querySelector(".modal-footer");
+  clearHTML(modalFooter);
+
+  const btnFavorite = document.createElement("button");
+  btnFavorite.classList.add("btn", "btn-danger", "col");
+  btnFavorite.textContent = "Guardar Favorito";
+  btnFavorite.onclick = function () {
+    addFavorite({
+      id: idMeal,
+      title: strMeal,
+      img: strMealThumb,
+    });
+  };
+
+  const btnCloseModal = document.createElement("button");
+  btnCloseModal.classList.add("btn", "btn-secondary", "col");
+  btnCloseModal.textContent = "Cerrar";
+  btnCloseModal.onclick = function () {
+    modal.hide();
+  };
+
+  modalFooter.appendChild(btnFavorite);
+  modalFooter.appendChild(btnCloseModal);
+
+  modal.show();
+}
+
+function clearHTML(select) {
+  while (select.firstChild) {
+    select.removeChild(select.firstChild);
   }
 }
